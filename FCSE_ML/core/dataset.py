67f6510c6c83c54fd96d4f7b86f49c5cd7f39137
NikolaP454 <97197562+NikolaP454@users.noreply.github.com>
2024-04-16 22:32:32 +0200

    New core

---
+++ FCSE_ML/core/dataset.py
@@ -0,0 +1,44 @@
+import pandas as pd
+
+class Dataset:
+    
+    def get_data(self, type: str='list'):
+        '''
+        Returns the data in the specified format.
+        
+        Parameters:
+            type (str): The format of the data to be returned. Default is 'list'.
+        '''
+        
+        raise NotImplementedError
+    
+    def head(self, number_of_rows: int=5):
+        '''
+        Returns the first n rows of the dataset.
+        
+        Parameters:
+            number_of_rows (int): The number of rows to return. Default is 5.
+        '''
+        
+        raise NotImplementedError
+    
+    def info(self):
+        '''
+        Returns the information about the dataset.
+        
+        Parameters:
+            None
+        '''
+        
+        raise NotImplementedError
+    
+    def load_data(self, df: pd.DataFrame):
+        '''
+        Loads the data into the dataset.
+        
+        Parameters:
+            df (pd.DataFrame): The data to be loaded.
+        '''
+        
+        pass
+    
